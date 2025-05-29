52abdcf5e44a1fbb5f25c6f7baaa3bd9290fc4d1
KimS1Yong <sdragon0928@naver.com>
2025-05-29 12:40:47 +0900

    push

--- src/main/java/org/example/bootcloudstep3/dto/MenuDTO.java
+++ src/main/java/org/example/bootcloudstep3/dto/MenuDTO.java
@@ -1,4 +1,4 @@
 package org.example.bootcloudstep3.dto;
 
-public record MenuDTO() {
+public record MenuDTO (String name) {
 }
--- src/main/java/org/example/bootcloudstep3/repository/MenuRepository.java
+++ src/main/java/org/example/bootcloudstep3/repository/MenuRepository.java
@@ -1,4 +1,7 @@
 package org.example.bootcloudstep3.repository;
 
-public interface MenuRepository {
+import org.example.bootcloudstep3.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
